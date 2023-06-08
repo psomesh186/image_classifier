e14a7dffc10b92e2e4a745314eabfea729906c5d
Somesh Pillay <psomesh186@gmail.com>
2023-06-08 18:06:01 -0400

    perf: change classifier model architecture

--- model.py
+++ model.py
@@ -10,73 +10,43 @@ class CNNClassifier(nn.Module):
         Args:
             num_labels (int): Number of classes present in dataset.    
         """
-        super().__init__()
-        self.block1 = nn.Sequential(
-            nn.Conv2d(in_channels=3, out_channels=8, kernel_size=7, bias=False),
-            nn.BatchNorm2d(num_features=8),
-            nn.LeakyReLU(),
-            nn.MaxPool2d(kernel_size=2, stride=2)
-        )
-        self.block2 = nn.Sequential(
+        super(CNNClassifier, self).__init__()
+        self.features = nn.Sequential(
             nn.Conv2d(
-                in_channels=8,
-                out_channels=16,
-                kernel_size=7,
-                stride=2,
-                bias=False
-                ),
-            nn.BatchNorm2d(num_features=16),
-            nn.LeakyReLU(),
-            nn.MaxPool2d(kernel_size=2, stride=2)
-        )
-        self.block3 = nn.Sequential(
-            nn.Conv2d(
-                in_channels=16,
+                in_channels=3,
                 out_channels=32,
-                kernel_size=3,
-                bias=False,
-                stride=2
+                kernel_size=5
                 ),
-            nn.BatchNorm2d(num_features=32),
-            nn.LeakyReLU(),
-            nn.MaxPool2d(kernel_size=2, stride=2)
-        )
-        self.block4 = nn.Sequential(
+            nn.ReLU(inplace=True),
+            nn.MaxPool2d(kernel_size=2, stride=2),
             nn.Conv2d(
                 in_channels=32,
                 out_channels=64,
-                kernel_size=3,
-                bias=False,
+                kernel_size=5,
                 ),
-            nn.BatchNorm2d(num_features=64),
-            nn.LeakyReLU(),
-            nn.MaxPool2d(kernel_size=2, stride=2)
-        )
-        self.block5 = nn.Sequential(
+            nn.ReLU(inplace=True),
+            nn.MaxPool2d(kernel_size=2, stride=2),
             nn.Conv2d(
                 in_channels=64,
                 out_channels=128,
-                kernel_size=2,
+                kernel_size=3,
                 ),
-            nn.BatchNorm2d(128),
-            nn.LeakyReLU()
+            nn.ReLU(inplace=True),
+            nn.MaxPool2d(kernel_size=2, stride=2),
+            nn.Conv2d(
+                in_channels=128,
+                out_channels=256,
+                kernel_size=5,
+                ),
+            nn.ReLU(inplace=True),
+            nn.MaxPool2d(kernel_size=2, stride=2)
         )
-        self.fc1 = nn.Sequential(
-            nn.Linear(in_features=128, out_features=512),
-            nn.LeakyReLU()
-        )
-        self.out = nn.Sequential(
-            nn.Linear(in_features=512, out_features=num_labels),
-            nn.Sigmoid()
-        )
+        self.avgpool = nn.AdaptiveAvgPool2d(output_size=1)
+        self.classifier = nn.Linear(in_features=256, out_features=num_labels)
 
     def forward(self, x):
-        x = self.block1(x)
-        x = self.block2(x)
-        x = self.block3(x)
-        x = self.block4(x)
-        x = self.block5(x)
-        x = x.squeeze()
-        x = self.fc1(x)
-        x = self.out(x)
+        x = self.features(x)
+        x = self.avgpool(x)
+        x = x.reshape(x.shape[0], -1)
+        x = self.classifier(x)
         return x
