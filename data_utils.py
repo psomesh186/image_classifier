b88830e68a083308e4e356d5077c22b6da0cae5b
Somesh Pillay <psomesh186@gmail.com>
2023-06-08 18:05:01 -0400

    feat: add dataloader parameters
    Allow parametric initialization of batch_size, num_workers and shuffle
    of the dataloader

--- data_utils.py
+++ data_utils.py
@@ -5,15 +5,21 @@ from torch.utils.data import Dataset, DataLoader
 from torchvision import datasets, transforms
 
 
-def get_dataloader(root_dir):
+def get_dataloader(root_dir, shuffle=True, batch_size=64, num_workers=4):
     """Creates dataloader for required task.
     Args:
-        root_dir (str): Directory with all images
-    
+        root_dir (str): Directory with all images.
+        shuffle (bool): Toggle dataloader shuffle. (Default: True)
+        batch_size (int): Batch size of the mini-batches. (Default: 64)
+        num_workers (int): Number of parallel workers. (Default: 4)
     Returns:
         A dataloader object.
     """
     transform = transforms.ToTensor()
     dataset = datasets.ImageFolder(root=root_dir, transform=transform)
-    dataloader = DataLoader(dataset, batch_size=32, shuffle=True)
+    dataloader = DataLoader(
+        dataset,
+        batch_size=batch_size,
+        shuffle=shuffle,
+        num_workers=num_workers)
     return dataloader
